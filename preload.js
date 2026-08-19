27af662edc38e602019aef37655981054a71f555
zakuorochi <53128002+zakuorochi@users.noreply.github.com>
2026-08-19 00:24:45 -0500

    Delete preload.js

--- preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  analizarTesis: (datosPdf) => ipcRenderer.invoke('analizar-tesis', datosPdf)
-});
